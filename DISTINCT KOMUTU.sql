b130f85192a240a9232c94552e1b62c7c5ae0c03
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-24 13:53:55 +0300

    Add files via upload

---
+++ DISTINCT KOMUTU.sql
@@ -0,0 +1,6 @@
+--DISTINCT KOMUTU
+--Sistem üzerinde sürekli tekrarlanan iþlemleri kýsýtlamak ve daha sade hale getirmek için kullanýlýr
+
+select distinct UrunAdý from urun
+select distinct UrunKategorisi from urun
+select distinct UrunNo from urun
