dadc0531adb1761e55e32ef9f4e333738a3ee103
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-24 14:14:57 +0300

    Add files via upload

---
+++ UPPER FONKSİYONU.sql
@@ -0,0 +1,6 @@
+--UPPER FONKSÝYONU
+--Küçük karakterleri büyük karakter haline dönüþtürmeye yarayan fonksiyondur
+
+select UPPER(UrunAdý) from urun
+select UPPER(UrunKategorisi) from urun
+select UPPER(UrunAdý) as [Ürün Adlarý] from urun
