20f8063c61a76238563f77c13799c2fcc0c85ff0
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-24 11:39:29 +0300

    Add files via upload

---
+++ INSERT KOMUTU.sql
@@ -0,0 +1,12 @@
+--INSERT KOMUTU
+--Veri eklemeyi saðlar
+--Veritabaný içinde herhangi bir tabloya veri eklemek için kullanýlan bir SQL DML (Data Manipulation Language) yani Veri Ýþleme Dili komutudur.
+
+Insert into urun(UrunNo,UrunAdý,UrunAdedi,UrunKategorisi)
+values(61,'Erik',600,'Meyve')
+select * from urun
+
+Insert into urun(UrunNo,UrunAdý,UrunAdedi,UrunKategorisi)
+values(62,'Yeni Dünya',420,'Meyve')
+select * from urun
+
