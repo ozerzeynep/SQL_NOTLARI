83ed5c3961502fea96a30c7c16756f9428941049
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-24 13:22:48 +0300

    Add files via upload

---
+++ DELETE KOMUTU.sql
@@ -0,0 +1,8 @@
+--DELETE KOMUTU
+--Silme iþlemi için kullanýlýr
+delete from urun
+delete from urun where UrunNo=13
+delete from urun where UrunKategorisi='Sebze'
+delete from urun where UrunAdedi>400
+delete from urun where UrunAdedi<230
+delete from urun where UrunAdý='Kereviz'
