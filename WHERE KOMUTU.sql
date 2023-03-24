022425675c206cca4828d335c1f3be8e5a01ecaa
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-24 13:26:03 +0300

    Add files via upload

---
+++ WHERE KOMUTU.sql
@@ -0,0 +1,8 @@
+--WHERE KOMUTU
+--Belirtilen koþula göre verileri filtrelemek için kullanýlýr
+
+select * from urun where UrunNo<25
+select * from urun where UrunKategorisi='Meyve'
+select * from urun where UrunAdedi>430
+select * from urun where UrunAdý='Elma'
+
