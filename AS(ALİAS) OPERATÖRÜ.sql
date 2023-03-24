5965b243de8c2ccafa02b3ea1b27ff8bebf98f46
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-24 13:47:28 +0300

    Add files via upload

---
+++ AS(ALİAS) OPERATÖRÜ.sql
@@ -0,0 +1,6 @@
+--AS(ALÝAS) OPERATÖRÜ
+--Sütunlarý farklý adlara dönüþtüre bilirsiniz
+
+select UrunAdý as[Ürün Adý] from urun
+select UrunKategorisi as[Ürün Kategorilerini Ýçerir] from urun
+select UrunNo as[Ürün Numaralarý] from urun
