f77cd60adc6f7a1d776f29bc18d5e5e98c8b5996
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-24 13:50:52 +0300

    Add files via upload

---
+++ BETWEEN KULLANIMI.sql
@@ -0,0 +1,6 @@
+--BETWEEN KULLANIMI
+--Ýfadenin deðerlerinin belirtilen deðer aralýðý içinde yer alýp almadýðýný saptar
+
+select * from urun where UrunAdedi between 100 and 500
+select * from urun where UrunAdedi between 500 and 600
+select * from urun where UrunNo between 20 and 60 
