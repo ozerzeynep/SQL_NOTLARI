6aca3990f8d9fbe020dccd69deb9248956847470
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-24 13:36:16 +0300

    Add files via upload

---
+++ OR OPERATÖRÜ.sql
@@ -0,0 +1,5 @@
+--OR OPERATÖRÜ
+--Koþullarýn birinin saðlanmasý yeterli olacaktýr
+
+select * from urun where UrunAdý='Lahana' or UrunKategorisi='Meyve'
+select * from urun where UrunNo>50 or UrunAdedi>=700
