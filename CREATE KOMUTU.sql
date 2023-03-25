121789ba4bda9bf73b10055b4e29710c9fbe64a1
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-25 13:55:25 +0300

    Add files via upload

---
+++ CREATE KOMUTU.sql
@@ -0,0 +1,10 @@
+--CREATE KOMUTU
+--Veritabaný oluþturmak için kullanýlýr
+
+create database okul
+use okul
+create table ogr(
+OgrNo int,
+OgrÝsim varchar(10),
+OgrSoyisim varchar(20),
+OgrTC char(11))
