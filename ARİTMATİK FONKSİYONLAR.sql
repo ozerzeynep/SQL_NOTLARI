4109d04a8ac2897dac6c7ecf74716914747a5774
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-25 14:30:42 +0300

    Add files via upload

---
+++ ARİTMATİK FONKSİYONLAR.sql
@@ -0,0 +1,4 @@
+--ARÝTMATÝK FONKSÝYONLAR
+
+select 20+4 as 'Toplama', 20-4 as 'Çýkartma', 20*4 as 'Çarpma', 20/4 as 'Bölüm'
+select UrunAdý +  '  -  '  + UrunKategorisi as 'Ürünler ve Türleri' from urun
