155c15efdbdf8fbd10073b95f96aa17883b96711
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-25 13:38:36 +0300

    Add files via upload

---
+++ SUBSTRİNG FONKSİYONU.sql
@@ -0,0 +1,5 @@
+--SUBSTRÝNG FONKSÝYONU
+--Ýstediðimiz karakter aralýðýný çekip almamýzý saðlar
+select SUBSTRING(UrunAdý,0,4) from urun
+select SUBSTRING(UrunKategorisi,1,8) from urun
+select SUBSTRING(UrunAdý,4,8) from urun
