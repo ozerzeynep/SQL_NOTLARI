fc531ecf35837c76fb91935e36e49dc2f5f50f2a
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-25 13:50:07 +0300

    Add files via upload

---
+++ TOP KOMUTU.sql
@@ -0,0 +1,7 @@
+--TOP KOMUTU
+--Çaðýrýlacak verilerin sayýsýný belirtmek için kullanýlýr
+
+select Top 10 * from urun
+select Top 5 * from urun
+select Top 12 UrunAdý from urun
+select Top 3 UrunKategorisi from urun
