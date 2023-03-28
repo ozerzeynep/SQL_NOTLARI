5eeb7b453adb6e700ae4598b4bc99a069c7c175c
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-28 18:00:25 +0300

    Add files via upload

---
+++ FLOOR FONKSİYONU.sql
@@ -0,0 +1,6 @@
+--FLOOR FONKSÝYONU
+--Verilen sayýsal ifadeyi kendisine en yakýn ve kendisinden en küçük olan tam sayýya yuvarlayan bir fonksiyondur
+
+select FLOOR(UrunAdedi) from urun
+select FLOOR(UrunNo) from urun
+
