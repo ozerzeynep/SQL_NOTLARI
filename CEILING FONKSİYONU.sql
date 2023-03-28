51f994209908af0020f3b476c891bfb699a419d3
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-28 17:57:57 +0300

    Add files via upload

---
+++ CEILING FONKSİYONU.sql
@@ -0,0 +1,6 @@
+--CEILING FONKSÝYONU
+--Verilen sayýsal ifadeyi kendisine en yakýn ve kendisinden büyük olan tam sayýya yuvarlayan bir fonksiyondur
+--Virgüllü deðerlerde kullanýlýr
+
+select CEILING(UrunAdedi) from urun
+select CEILING(UrunNo) from urun
