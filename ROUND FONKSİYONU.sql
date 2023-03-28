32f7563607ef71d3252271e80d93a6ae67696153
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-28 18:03:05 +0300

    Add files via upload

---
+++ ROUND FONKSİYONU.sql
@@ -0,0 +1,4 @@
+--ROUND FONKSÝYONU
+--Verilen sayýnýn, verilen basamaðýndan sonrasýnýn yakýn tarafa yuvarlanmasýný saðlar. Virgülden sonra kaç basamak geleceðini belirler
+
+select UrunAdý, UrunAdedi, ROUND(UrunAdedi,1) from urun
