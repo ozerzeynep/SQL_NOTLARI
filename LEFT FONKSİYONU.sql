f5d6099ac58e1bd4e47c27b13d8613ea51b2684e
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-28 17:35:53 +0300

    Add files via upload

---
+++ LEFT FONKSİYONU.sql
@@ -0,0 +1,5 @@
+--LEFT FONKSÝYONU
+--Metinsel ifadelerin baþ kýsmýndan yani sol kýsmýndan baþlayýp belirtilen uzunluktaki kýsmýný döndürür
+
+select LEFT(UrunAdý,4) as 'Left Fonksiyonu Kullanýmý' from urun
+select LEFT(UrunKategorisi,6) from urun
