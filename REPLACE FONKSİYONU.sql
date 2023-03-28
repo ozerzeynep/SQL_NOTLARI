8d4e2fda5f13286f3da3bd686b215c12591ab0d1
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-03-28 17:49:25 +0300

    Add files via upload

---
+++ REPLACE FONKSİYONU.sql
@@ -0,0 +1,4 @@
+--REPLACE FONKSÝYONU
+--String ifade içindeki bir alanýn istenildiði gibi deðiþtirilmesinde kullanýlan bir fonksiyondur
+
+select UrunAdý, REPLACE(UrunKategorisi,'Baklagil','Baklaa') from urun
