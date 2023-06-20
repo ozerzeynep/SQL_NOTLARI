1a7e5eae1c59e8a04e023d053abbcc3e83575e19
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-06-20 12:42:33 +0300

    Update SUBSTRİNG FONKSİYONU.sql

--- SUBSTRİNG FONKSİYONU.sql
+++ SUBSTRİNG FONKSİYONU.sql
@@ -1,5 +1,6 @@
 --SUBSTRING FONKSİYONU
 --İstediğimiz kararkter aralığını çekip almamızı sağlar.
+--Üç parametre alır: KOLON , BAŞLANGIÇ , UZUNLUK
 select SUBSTRING(UrunAdı,0,4) from urun
 select SUBSTRING(UrunKategorisi,1,8) from urun
 select SUBSTRING(UrunAdı,4,8) from urun
