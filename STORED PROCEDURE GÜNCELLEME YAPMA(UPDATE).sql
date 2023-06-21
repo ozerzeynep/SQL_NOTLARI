557b866f20656f859faa145ddbe52c5885882e88
Zeynep Özer <100872384+ozerzeynep@users.noreply.github.com>
2023-06-21 15:26:28 +0300

    Update STORED PROCEDURE GÜNCELLEME YAPMA(UPDATE).sql

--- STORED PROCEDURE GÜNCELLEME YAPMA(UPDATE).sql
+++ STORED PROCEDURE GÜNCELLEME YAPMA(UPDATE).sql
@@ -1,10 +1,10 @@
---STORED PROCEDURE GÜNCELLEME YAPMA(UPDATE)
+--STORED PROCEDURE GÃœNCELLEME YAPMA(UPDATE)
 create proc sp_guncelle
 as
 begin
-	update firma set CalýsanBolumu='Yönetim Biliþim Sistemleri' where CalýsanID=40
+	update firma set CalsianBolumu='YÃ¶netim BiliÅŸim Sistemleri' where CalisanID=40
 end
 
 exec sp_guncelle
-exec sp_calýsan
+exec sp_calisan
 
